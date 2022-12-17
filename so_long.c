4b74c25054e680f8b6eb4e7a1e38c119ebdaf916
Walid Araissi <waraissi@e2r3p4.1337.ma>
2022-12-17 11:50:37 +0100

    base

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 17:52:19 by waraissi          #+#    #+#             */
-/*   Updated: 2022/12/16 17:59:20 by waraissi         ###   ########.fr       */
+/*   Updated: 2022/12/17 11:49:25 by waraissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,11 @@ int map_height(char **matrix)
     int i;
 
     i = 0;
-    while (matrix && *matrix && matrix[i])
+    while (matrix[i])
         i++;
     return (i);
 }
 
-int check_first_element(char *str)
-{
-    int counter;
-
-    counter = 0;
-    if (str[0] == '0')
-        counter++; 
-    return (counter);
-}
-
-int check_last_element(char *str)
-{
-    int counter;
-    int i;
-
-    i = ft_strlen(str);
-    counter = 0;
-    if (str[i] == '0')
-        counter++;
-    return (counter);
-}
 char **surrounded_map(char **matrix)
 {
     int i = 0;
@@ -67,20 +46,20 @@ char **surrounded_map(char **matrix)
     }
     return (matrix);
 }
+
 char **read_map(int fd)
 {
     char *get_lines;
     char *lines;
-    t_data  *data;
+    t_data  data;
     char **matrix;
     int i;
 
     i = 0;
     lines = NULL;
-    data = malloc(sizeof(t_data));
-    data->C = 0;
-    data->E = 0;
-    data->P = 0;
+    data.C = 0;
+    data.E = 0;
+    data.P = 0;
     while (1)
     {
         get_lines = get_next_line(fd);
@@ -91,14 +70,14 @@ char **read_map(int fd)
     while (lines[i])
     {
         if (lines[i] == 'C')
-            data->C = data->C + 1;
+            data.C = data.C + 1;
         else if (lines[i] == 'P')
-            data->P = data->P + 1;
+            data.P = data.P + 1;
         else if (lines[i] == 'E')
-            data->E = data->E + 1;
+            data.E = data.E + 1;
         i++;
     }
-    if (data->P != 1 || data->E != 1 || data->C < 1)
+    if (data.P != 1 || data.E != 1 || data.C < 1)
     {
         write(1, "Error ok",9);
         exit(1);
@@ -106,17 +85,10 @@ char **read_map(int fd)
     matrix = ft_split(lines,'\n');
     surrounded_map(matrix);
     // func
-    data->height = map_height(matrix);
-    i = 0;
-    while (matrix[0][i])
-    {
-        if(ft_strchr(matrix[0],'0'))
-        {
-            write(1, "Error (found 0 in the first line)\n", 36);
-            exit(1);
-        }
-        i++;
-    }
+    data.height = map_height(matrix);
+    printf("%d\n",data.height);
+    if(ft_strchr(matrix[0],'0') || ft_strchr(matrix[data.height - 1],'0'))
+        return (write(1, "Error (found 0)\n", 17), NULL);
     return matrix;
 }
 
